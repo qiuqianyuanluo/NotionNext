902c73978e687d3dd841e4621756336fa42e174f
qiuqianyuanluo <qiuqianyuanluo@126.com>
2025-02-24 13:13:19 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -1,17 +1,22 @@
 /**
  * 网站字体相关配置
- * 
+ *
  */
 module.exports = {
   // START ************网站字体*****************
-  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif font-normal',
-
+  // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
+  // 后面空格隔开的font-light的字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
+  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-light',
+  // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
-    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;700&display=swap',
-    'https://fonts.googleapis.com/css?family=Bitter&display=swap'
+    // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
+    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
+    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
   ],
-
+  // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
+    // '"LXGW WenKai"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
@@ -29,10 +34,11 @@ module.exports = {
     'sans-serif',
     '"Apple Color Emoji"'
   ],
-
+  // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
-    '"Noto Serif SC"',
+    // '"LXGW WenKai"',
     'Bitter',
+    '"Noto Serif SC"',
     'SimSun',
     '"Times New Roman"',
     'Times',
@@ -41,20 +47,9 @@ module.exports = {
     '"Segoe UI Symbol"',
     '"Apple Color Emoji"'
   ],
-
   FONT_AWESOME:
     process.env.NEXT_PUBLIC_FONT_AWESOME_PATH ||
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
 
-  // 关键修复：添加逗号
-  GLOBAL_CSS: `
-    :root {
-      --article-font-size: 1.5rem;
-    }
-    .notion {
-      font-size: var(--article-font-size) !important;
-      font-family: "Noto Serif SC", serif;
-    }
-  `
   // END ************网站字体*****************
 }
